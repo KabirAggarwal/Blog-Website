1dcf4fc5c39f92f308e0253f86a0e6b4c91ac868
Kabir Aggarwal <55024992+kabira789@users.noreply.github.com>
2020-12-19 22:33:00 +0530

    Upto challenge 8

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require("body-parser");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -9,6 +10,8 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
@@ -19,7 +22,22 @@ app.get('/', (req, res) => {
 })
 
 
+app.get('/about', (req, res) => {
+  res.render('about',{aboutContent})
+})
+app.get('/contact', (req, res) => {
+  res.render('contact',{contactContent})
+})
 
+app.get('/compose',(req,res)=>{
+  res.render('compose')
+})
+
+app.post('/compose',(req,res)=>{
+  console.log(req.body.postTitle)
+  res.render('compose')
+
+})
 
 
 
