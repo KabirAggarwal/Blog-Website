d8b180cbef73a5addfe69429dc9610dfc5d48d11
Kabir Aggarwal <55024992+kabira789@users.noreply.github.com>
2020-12-20 20:08:44 +0530

    edits

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-
+var posts=[];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
-    res.render('home',{homeStartingContent})
+    res.render('home',{homeStartingContent,posts})
 })
 
 
@@ -34,9 +34,17 @@ app.get('/compose',(req,res)=>{
 })
 
 app.post('/compose',(req,res)=>{
-  console.log(req.body.postTitle)
-  res.render('compose')
+  var post={
+    title: req.body.postTitle,
+    body: req.body.postBody
+  }
+  posts.push(post);
+  res.redirect('/')
 
+})
+
+app.get('/posts/:postName',(req,res)=>{
+  console.log(req.params.postName);
 })
 
 
