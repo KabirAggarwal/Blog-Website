e33d2f46f52c190ae28966150d21409ac1bfbad4
Kabir Aggarwal <55024992+kabira789@users.noreply.github.com>
2020-12-20 21:57:54 +0530

    Complete Project

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -9,6 +9,8 @@ var path = require('path');
 
 var bodyParser = require("body-parser");
 
+var _ = require("lodash");
+
 var homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 var aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 var contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -48,7 +50,18 @@ app.post('/compose', function (req, res) {
   res.redirect('/');
 });
 app.get('/posts/:postName', function (req, res) {
-  console.log(req.params.postName);
+  var check = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    var pTitle = _.lowerCase(post.title);
+
+    if (pTitle === check) {
+      res.render('post', {
+        title: post.title,
+        content: post.body
+      });
+    }
+  });
 });
 app.listen(3000, function () {
   console.log("Server started on port 3000");
